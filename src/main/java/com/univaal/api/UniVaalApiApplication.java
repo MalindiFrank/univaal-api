f56f5716f0dfb9008697f6083fd58acd0be9d74a
thmalinjhb024@ <thmalinjhb024@>
2026-01-16 18:24:56 +0200

    initial project setup:

---
+++ src/main/java/com/univaal/api/UniVaalApiApplication.java
@@ -0,0 +1,13 @@
+package com.univaal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniVaalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniVaalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univaal/api/UniVaalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univaal.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniVaalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
