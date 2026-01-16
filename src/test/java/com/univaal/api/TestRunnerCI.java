880321b3895acd141b2b02670163165cb51d6ece
thmalinjhb024@ <thmalinjhb024@>
2026-01-16 21:23:18 +0200

    Setup a TestRunner Class to ensure CI Gate works. Added the JUnit (5.10.2) dependency Added aven-surefire-plugin (3.2.5)

---
+++ src/test/java/com/univaal/api/TestRunnerCI.java
@@ -0,0 +1,14 @@
+package com.univaal.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestRunnerCI {
+
+    @Test
+    void TestRunnerForCIGate() {
+        assertTrue(true);
+    }
+
+}
