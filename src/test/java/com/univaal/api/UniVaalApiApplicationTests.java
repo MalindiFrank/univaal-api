c5a35c6e1feab42240847be932a272ffc06d70b8
thmalinjhb024@ <thmalinjhb024@>
2026-01-16 20:58:31 +0200

    Setup a TestRunner to ensure CI Gate works. Added the JUnit (5.10.2) dependency Added aven-surefire-plugin (3.2.5)

--- src/test/java/com/univaal/api/UniVaalApiApplicationTests.java
+++ src/test/java/com/univaal/api/UniVaalApiApplicationTests.java
@@ -3,11 +3,18 @@ package com.univaal.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class UniVaalApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void TestRunnerForCIGate() {
+        assertTrue(true);
+    }
 
 }
